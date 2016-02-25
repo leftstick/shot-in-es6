73ebabf41b2b2747c77a9468379cc6177eecd6f9
Howard.Zuo <leftstick@qq.com>
2016-02-25 13:08:06 +0800

    add new case of set

--- test/proxy/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var expect = require('chai').expect;
-
-describe('Proxy', function() {
-
-    it('to be done', function() {});
-
-});
---
+++ test/set/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+var expect = require('chai').expect;
+
+describe('Set', function() {
+
+    it('unique', function() {
+        var cities = new Set(['Shanghai', 'Beijing', 'Guangzhou']);
+
+        expect(cities.size).to.be.equal(3);
+
+        cities.add('Shanghai');
+
+        expect(cities.size).to.be.equal(3);
+    });
+
+    it('contains', function() {
+        var cities = new Set(['Shanghai', 'Beijing', 'Guangzhou']);
+
+        expect(cities.has('Shanghai')).to.be.equal(true);
+
+    });
+
+    it('delete', function() {
+        var cities = new Set(['Shanghai', 'Beijing', 'Guangzhou']);
+
+        cities.delete('Beijing');
+
+        expect(cities.has('Beijing')).to.be.equal(false);
+        expect(cities.size).to.be.equal(2);
+
+    });
+
+    it('clear', function() {
+        var cities = new Set(['Shanghai', 'Beijing', 'Guangzhou']);
+
+        cities.clear('Beijing');
+
+        expect(cities.size).to.be.equal(0);
+
+    });
+
+});
